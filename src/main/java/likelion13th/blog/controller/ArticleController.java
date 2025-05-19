374c936d14f3136326d37487583010157b477c41
Jongyun Won <jywon1128@gmail.com>
2025-05-19 19:35:46 +0900

    Refactor: 게시글 생성 및 전체 조회 로직 리팩토링

--- src/main/java/likelion13th/blog/controller/ArticleController.java
+++ src/main/java/likelion13th/blog/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package likelion13th.blog.controller;
 
 import likelion13th.blog.domain.Article;
+import likelion13th.blog.service.ArticleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,31 +12,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
-    private final List<Article> articleDB = new ArrayList<>();
-    private Long nextId = 1L;
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @PostMapping()
     public ResponseEntity<Article> createArticle(@RequestBody Article article) {
 
-        Article newArticle = new Article(
-                nextId++,
-                article.getTitle(),
-                article.getContent(),
-                article.getAuthor(),
-                article.getPassword()
-        );
-
-        articleDB.add(newArticle);
+        Article newArticle = articleService.addArticle(article);
 
         return ResponseEntity
-                .status(HttpStatus.OK)
+                .status(HttpStatus.CREATED)
                 .body(newArticle);
     }
 
     @GetMapping()
     public ResponseEntity<List<Article>> getArticles() {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleDB);
+        return ResponseEntity.ok(articleService.findAll());
     }
 }
---
+++ src/main/java/likelion13th/blog/service/ArticleService.java
@@ -0,0 +1,40 @@
+package likelion13th.blog.service;
+
+import likelion13th.blog.domain.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    private final List<Article> articleDB = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public Article addArticle(Article article) {
+
+        if(article.getAuthor() == null
+        || article.getContent() == null
+        || article.getTitle() == null
+        || article.getPassword() == null) {
+            throw new IllegalArgumentException("제목, 내용, 작성자, 비밀번호는 필수 입력사항입니다.");
+        }
+
+        Article newArticle = new Article(
+                nextId++,
+                article.getTitle(),
+                article.getContent(),
+                article.getAuthor(),
+                article.getPassword()
+        );
+
+        articleDB.add(newArticle);
+
+        return newArticle;
+    }
+
+    public List<Article> findAll() {
+        return articleDB;
+    }
+}
